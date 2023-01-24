bfda07dad970782f67d711804dfbb4e26a3f12f4
Michael Bryant <mbryant@rigetti.com>
2023-01-23 16:09:40 -0800

    chore(python): remove unnecessary comments

--- crates/python/src/lib.rs
+++ crates/python/src/lib.rs
@@ -8,10 +8,6 @@ pub mod grpc;
 pub mod qpu;
 pub mod register_data;
 
-// pub use executable::{Error, Executable, ExecuteResultQPU, ExecuteResultQVM, JobHandle, Service};
-// pub use execution_data::{Qpu, Qvm, ReadoutMap};
-// pub use register_data::RegisterData;
-
 create_init_submodule! {
     classes: [
         execution_data::PyQpu,
