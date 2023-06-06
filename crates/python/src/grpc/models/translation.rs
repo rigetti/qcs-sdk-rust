6ce00781431e0d31cddcbde6c7d054945ec77c7c
Jake Selig <jselig@rigetti.com>
2023-06-06 12:16:43 +0900

    chore: add constructors for grpc types

--- crates/python/src/grpc/models/translation.rs
+++ crates/python/src/grpc/models/translation.rs
@@ -1,16 +1,35 @@
 use qcs_api_client_grpc::services::translation::{
     translation_options::TranslationBackend, BackendV1Options, BackendV2Options, TranslationOptions,
 };
-use rigetti_pyo3::{py_wrap_data_struct, py_wrap_type, py_wrap_union_enum, create_init_submodule};
+use rigetti_pyo3::{
+    create_init_submodule, py_wrap_data_struct, py_wrap_type, py_wrap_union_enum,
+    pyo3::{pymethods, PyResult},
+};
 
 py_wrap_type! {
     #[derive(Default)]
     PyBackendV1Options(BackendV1Options) as "BackendV1Options";
 }
 
+#[pymethods]
+impl PyBackendV1Options {
+    #[new]
+    fn __new__() -> PyResult<Self> {
+        Ok(Self::default())
+    }
+}
+
 py_wrap_type! {
     #[derive(Default)]
     PyBackendV2Options(BackendV2Options) as "BackendV2Options";
+}
+
+#[pymethods]
+impl PyBackendV2Options {
+    #[new]
+    fn __new__() -> PyResult<Self> {
+        Ok(Self::default())
+    }
 }
 
 py_wrap_union_enum! {
@@ -24,6 +43,16 @@ py_wrap_data_struct! {
     #[derive(Default)]
     PyTranslationOptions(TranslationOptions) as "TranslationOptions" {
         translation_backend: Option<TranslationBackend> => Option<PyTranslationBackend>
+    }
+}
+
+#[pymethods]
+impl PyTranslationOptions {
+    #[new]
+    fn __new__(translation_backend: Option<PyTranslationBackend>) -> PyResult<Self> {
+        Ok(Self(TranslationOptions {
+            translation_backend: translation_backend.map(|b| b.0),
+        }))
     }
 }
 
