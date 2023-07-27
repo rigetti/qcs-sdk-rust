d95c837e9ded927126580f0d9a804ff45d5b0a77
Marquess Valdez <marquessavaldez@gmail.com>
2023-07-27 10:09:28 -0700

    rename to get_diagnostics_report
    Co-authored-by: Kalan <22137047+kalzoo@users.noreply.github.com>

--- crates/python/qcs_sdk/__init__.py
+++ crates/python/qcs_sdk/__init__.py
@@ -26,4 +26,4 @@ platform: {sys.platform}
 
 
 __doc__ = qcs_sdk.__doc__
-__all__ = ["gather_diagnostics"] + getattr(qcs_sdk, "__all__", [])
+__all__ = ["get_diagnostics_report"] + getattr(qcs_sdk, "__all__", [])
