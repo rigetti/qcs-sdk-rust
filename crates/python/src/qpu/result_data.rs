57a514ce7ed13f83974b73964621400803507336
marquessv <marquessavaldez@gmail.com>
2023-08-14 10:19:43 -0700

    feat(python): QpuResultData now has an `asdict()` method for retrieving the results as a flattened hashmap

--- crates/python/src/qpu/result_data.rs
+++ crates/python/src/qpu/result_data.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 
 use pyo3::{
     pymethods,
-    types::{PyComplex, PyFloat, PyInt},
-    Py, PyResult, Python,
+    types::{PyComplex, PyDict, PyFloat, PyInt},
+    Py, PyAny, PyResult, Python, ToPyObject,
 };
 use qcs::qpu::{QpuResultData, ReadoutValues};
 use rigetti_pyo3::{py_wrap_type, py_wrap_union_enum, PyTryFrom, PyWrapper, ToPython};
@@ -42,5 +42,28 @@ impl PyQpuResultData {
     #[getter]
     fn readout_values(&self, py: Python<'_>) -> PyResult<HashMap<String, PyReadoutValues>> {
         self.as_inner().readout_values().to_python(py)
+    }
+
+    fn asdict(&self, py: Python<'_>) -> PyResult<Py<PyDict>> {
+        let dict = PyDict::new(py);
+        dict.set_item("mappings", self.mappings(py)?)?;
+        dict.set_item(
+            "readout_values",
+            self.as_inner()
+                .readout_values()
+                .iter()
+                .map(|(register, values)| {
+                    (
+                        register.to_string(),
+                        match values {
+                            ReadoutValues::Integer(values) => values.to_object(py),
+                            ReadoutValues::Real(values) => values.to_object(py),
+                            ReadoutValues::Complex(values) => values.to_object(py),
+                        },
+                    )
+                })
+                .collect::<HashMap<String, Py<PyAny>>>(),
+        )?;
+        Ok(dict.into())
     }
 }
