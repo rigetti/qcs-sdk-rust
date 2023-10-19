a51fa39ec5f0ff1372dd50d69ebdb3ea1191b8bc
jselig-rigetti <97701976+jselig-rigetti@users.noreply.github.com>
2023-10-19 16:19:02 -0600

    feat: make Executable.get_qcs_client public (#380)
    * feat: make Executable.get_qcs_client public
    
    * Update crates/lib/src/executable.rs
    
    Co-authored-by: Michael Bryant <mbryant@rigetti.com>
    
    * chore: follow C-GETTER naming convention advice
    
    ---------
    
    Co-authored-by: Michael Bryant <mbryant@rigetti.com>

--- crates/lib/src/executable.rs
+++ crates/lib/src/executable.rs
@@ -299,14 +299,16 @@ impl<'executable> Executable<'executable, '_> {
         self
     }
 
-    /// Load `self.client` if not yet loaded, then return a reference to it.
-    async fn get_qcs_client(&mut self) -> Result<Arc<Qcs>, Error> {
+    /// Get a reference to the [`Qcs`] client used by the executable.
+    ///
+    /// If one has not been set, a default client is loaded, set, and returned.
+    pub async fn qcs_client(&mut self) -> Arc<Qcs> {
         if let Some(client) = &self.qcs_client {
-            Ok(client.clone())
+            client.clone()
         } else {
             let client = Arc::new(Qcs::load().await);
             self.qcs_client = Some(client.clone());
-            Ok(client)
+            client
         }
     }
 }
@@ -412,7 +414,7 @@ impl<'execution> Executable<'_, 'execution> {
             self.quil.clone(),
             self.shots,
             id,
-            self.get_qcs_client().await?,
+            self.qcs_client().await,
             self.quilc_client.clone(),
             self.compiler_options,
         )
@@ -852,7 +854,7 @@ mod describe_qpu_for_id {
                 "".into(),
                 shots,
                 "Aspen-M-3".into(),
-                exe.get_qcs_client().await.expect("should have client"),
+                exe.qcs_client().await,
                 exe.quilc_client.clone(),
                 CompilerOpts::default(),
             )
