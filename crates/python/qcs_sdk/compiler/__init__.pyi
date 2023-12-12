a698a5b81dafc128bdab3a9d11fd775872b7742f
Marquess Valdez <marquessavaldez@gmail.com>
2023-12-12 11:28:48 -0800

    fix stubtest error

--- crates/python/qcs_sdk/compiler/__init__.pyi
+++ crates/python/qcs_sdk/compiler/__init__.pyi
@@ -4,3 +4,5 @@ Module for interacting with the Quil Compiler (quilc).
 🔨 This page is under construction. In the meantime, you can find documentation
 in the [the type hints](https://github.com/rigetti/qcs-sdk-rust/blob/main/crates/python/qcs_sdk/compiler/quilc.pyi).
 """
+
+from qcs_sdk.compiler import *
