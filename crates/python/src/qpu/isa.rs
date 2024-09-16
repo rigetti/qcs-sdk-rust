53c03e0511660a2f3b831a6019f1aae9f7c8adaf
Marquess Valdez <marquessavaldez@gmail.com>
2024-09-16 15:50:15 -0700

    Update qcs-api-client-rust packages

--- crates/python/src/qpu/isa.rs
+++ crates/python/src/qpu/isa.rs
@@ -4,7 +4,7 @@ use qcs_api_client_openapi::models::{
 };
 use rigetti_pyo3::{
     create_init_submodule, py_function_sync_async, py_wrap_data_struct, py_wrap_error,
-    py_wrap_simple_enum,
+    py_wrap_union_enum,
     pyo3::{
         exceptions::{PyRuntimeError, PyValueError},
         prelude::*,
@@ -46,12 +46,13 @@ py_wrap_error!(isa, RustSerializeIsaError, SerializeISAError, PyValueError);
 wrap_error!(RustGetIsaError(qcs::qpu::GetIsaError));
 py_wrap_error!(isa, RustGetIsaError, GetISAError, PyRuntimeError);
 
-py_wrap_simple_enum! {
+py_wrap_union_enum! {
     PyFamily(Family) as "Family" {
-        None as NONE,
-        Full as Full,
-        Aspen as Aspen,
-        Ankaa as Ankaa
+        NONE: None,
+        Full: Full,
+        Aspen: Aspen,
+        Ankaa: Ankaa,
+        Unknown: Unknown => String
     }
 }
 
