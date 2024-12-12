35fec638f10c3dd6888a3f9ec221c1e50d3709cf
Eric Hulburd <ehulburd@rigetti.com>
2024-12-12 09:36:03 -0800

    docs: fix function import

--- crates/lib/src/qpu/experimental/random.rs
+++ crates/lib/src/qpu/experimental/random.rs
@@ -300,7 +300,7 @@ fn prng_value_to_sub_region_index(value: u64, sub_region_count: u8) -> u8 {
 /// with a seed of 639,523, you could backout the randomly chosen sub-regions with the following:
 ///
 /// ```rust
-/// use qcs::qpu::experimental::random::choose_random_real_sub_regions;
+/// use qcs::qpu::experimental::random::choose_random_real_sub_regions_indices;
 ///
 /// let seed = 639_523;
 /// let start_index = 0;
