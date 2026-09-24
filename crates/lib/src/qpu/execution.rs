d4442a7c575cf522fec19d621b568bf6f56c568a
Kyle J Strand <kstrand@rigetti.com>
2026-09-24 15:48:20 -0600

    submit-to-target doesn't need separate endpoint_id arg

--- crates/lib/src/qpu/execution.rs
+++ crates/lib/src/qpu/execution.rs
@@ -182,7 +182,6 @@ impl<'a> Execution<'a> {
 
         self.submit_to_target(
             params,
-            None,
             Some(&self.quantum_processor_id.clone()),
             translation_options,
             execution_options,
@@ -200,11 +199,9 @@ impl<'a> Execution<'a> {
     where
         S: Into<Cow<'a, str>>,
     {
-        let endpoint_id: Cow<'a, str> = endpoint_id.into();
-        let connection_strategy = ConnectionStrategy::EndpointId(endpoint_id.clone().into_owned());
+        let connection_strategy = ConnectionStrategy::EndpointId(endpoint_id.into().to_string());
         self.submit_to_target(
             params,
-            Some(&endpoint_id),
             None,
             translation_options,
             &ExecutionOptionsBuilder::default()
@@ -218,7 +215,6 @@ impl<'a> Execution<'a> {
     async fn submit_to_target(
         &mut self,
         params: &Parameters,
-        endpoint_id: Option<&str>,
         quantum_processor_id: Option<&str>,
         translation_options: Option<TranslationOptions>,
         execution_options: &ExecutionOptions,
@@ -235,10 +231,16 @@ impl<'a> Execution<'a> {
         )
         .await?;
 
+        let endpoint_id = match execution_options.connection_strategy() {
+            ConnectionStrategy::EndpointId(endpoint_id) => Some(endpoint_id.clone()),
+            ConnectionStrategy::Gateway { endpoint_id, .. } => endpoint_id.clone(),
+            ConnectionStrategy::DirectAccess() | ConnectionStrategy::EndpointAddress(_) => None,
+        };
+
         Ok(JobHandle::new(
             job_id,
             self.quantum_processor_id.to_string(),
-            endpoint_id.map(ToOwned::to_owned),
+            endpoint_id,
             readout_map,
             execution_options.clone(),
         ))
